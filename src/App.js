cfe958c7dcc50f50e5ac6557359222e27a1decbb
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-11-09 13:30:38 -0800

    added social links

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import Home from "./Home";
 import Projects from "./Projects";
 import About from "./About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function App() {
+  const linkedIn = () => {
+    window.location.href = "https://www.linkedin.com/in/phillip-laub/";
+    return null;
+  };
+
+  const gitHub = () => {
+    window.location.href = "https://github.com/PhillipLaub/";
+
+    return null;
+  };
   return (
     <Router>
       <div className="app">
+        <div className="app__left">
+          <div className="app__icons">
+            <LinkedInIcon onClick={linkedIn} className="app__social" />
+            <GitHubIcon onClick={gitHub} className="app__social" />
+          </div>
+        </div>
         <Switch>
           <Route exact path="/">
             <Header />
--- src/Projects.js
+++ src/Projects.js
@@ -1,99 +1,23 @@
 import React from "react";
 import "./Projects.css";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Home_desktop from "./images/Home_desktop.png";
 import ProjectCarousel from "./ProjectCarousel";
 
 function Projects() {
   return (
-    <div className="projects">
-      <div className="projects__container">
-        <div className="projects__left">
-          <div className="projects__name">
-            <h1>Projects</h1>
-          </div>
+    <div className="about">
+      <div className="projects__right">
+        <h1>right </h1>
+      </div>
+      <div class="diagonal-box">
+        <div className="banner--fadeTop"></div>{" "}
+        <div class="content">
+          <h1>Projects</h1>
         </div>
-        <div className="projects__right">
+        <div className="banner__bottom"></div>
+        <div className="about__text2">
           <ProjectCarousel />
-          {/* <div className="projects__row">
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src={Home_desktop} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className="projects__row">
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-
-          <div className="projects__row">
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: "18rem" }} className="projects__card">
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div> */}
         </div>
+        <div className="banner--fadeBottom"></div>
       </div>
     </div>
   );
